b965985257607119eced51b5c30f6e4a71473848
Kylun-Serg <kylun1serg@gmail.com>
2020-01-08 15:41:40 +0200

    add section 2

--- app/js/main.js
+++ app/js/main.js
@@ -14,5 +14,13 @@ $(function(){
 		 asNavFor: '.header__slider',
 	});
 
+	$('.surf-slider').slick({
+		 slidesToShow: 4,
+		 slidesToScroll: 1,
+		 prevArrow:'<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="">',
+		 nextArrow:'<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="">',
+		
+	});
+
   
 });
